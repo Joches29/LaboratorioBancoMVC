3b4708e5e8efeb41bd8781a05fb65f68ebfd90cc
Lero <lesanchezro@est.utn.ac.cr>
2025-09-29 01:14:44 -0600

    Clase Cuenta,CuentaColones,CuentaDolares,CuentaDao🔥
    CuentaDao esta en desarrollo😭

---
+++ src/Modelo/CuentaColones.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author llean
+ */
+public class CuentaColones extends Cuenta {
+    
+    public CuentaColones(String numeroCuenta, Cliente titular) {
+        super(numeroCuenta, titular, "CRC");
+    }
+    
+}
---
+++ src/Modelo/CuentaDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author llean
+ */
+public class CuentaDAO {
+    private ArrayList<Cuenta> cuentas = new ArrayList<>();
+    private static int consecutivo = 1;
+    private static final String PREFIJO = "101";
+
+}
